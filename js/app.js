a1f4b16b94ef960156737fd5ecae71fb8b449bde
ali-lutfalla <ali,latfalla@gmail.com>
2024-07-08 09:39:32 +0300

    update my work

--- js/app.js
+++ js/app.js
@@ -1,19 +1,19 @@
 /*-------------- Constants -------------*/
 
-const ScoreMenu = {rounds: null,
-                   difficulty:null,
-                   operator:null,
-                   correctAnswers:null
-                   ,wrongAnswers:null
+const ScoreMenu = {rounds: undefined,
+                   difficulty:undefined,
+                   operator:undefined,
+                   correctAnswers:undefined
+                   ,wrongAnswers:undefined
 };
 
 const questions ={ question:[{
-                     number1:null,
-                     number2:null,
-                     operator:null,
-                     correctAnswer:null,
-                     difficulty:null,
-                     userAnswer:null}]
+                     number1:undefined,
+                     number2:undefined,
+                     operator:undefined,
+                     correctAnswer:undefined,
+                     difficulty:undefined,
+                     userAnswer:undefined}]
 };
 
 const operators =['+','-','*','/'];
@@ -27,6 +27,18 @@ let currentQuestion = 0;
 
 
 /*----- Cached Element References  -----*/
+
+const startMenu = document.querySelector('.startMenu');
+const message = document.querySelector('.message');
+const rounds = document.querySelector('.rounds');
+const questionNumber = document.querySelector('.questionNumber');
+const questionMessage = document.querySelector('.questionMessage');
+const answersButtons = document.querySelectorAll('.answer');
+const questionSection = document.querySelector('.questionDiv');
+const endGameCard = document.querySelector('.gameResults');
+const endGameMessage = document.querySelector('.endGameMessage');
+const endGameDifficulty = document.querySelector('.endGameDifficulty');
+const endGameOperator = document.querySelector('.endGameOperator');
 
 
 /*-------------- Functions -------------*/
@@ -49,7 +61,7 @@ const calculator = (number1,number2,operator) => {
             return number1 - number2;
             break;
         case '+':
-            return number1 - number2;
+            return number1 + number2;
             break;
         case '*':
             return number1 * number2;
@@ -61,10 +73,10 @@ const calculator = (number1,number2,operator) => {
 
 const generateQuestion = () => {
     const question = {number1:null,
-        number2:null,
-        operator:null,
-        difficulty:null,
-        correctAnswer:null};
+        number2:undefined,
+        operator:undefined,
+        difficulty:undefined,
+        correctAnswer:undefined};
     if (ScoreMenu.difficulty === 'mix'){
         question.difficulty = guessDifficulty();
     } else {
@@ -103,4 +115,91 @@ const generateQuestions = () => {
     }
 }
 
+const questionNumberMessage = () => {
+    return `Question Number ${currentQuestion+1}:`;
+}
+
+const questionMessagePrint = () => {
+    return `The question is ${questions.question[currentQuestion].number1} ${questions.question[currentQuestion].operator} ${questions.question[currentQuestion].number2} `;
+}
+
+const generateRandomAnswers = () => {
+    answersButtons.forEach((element) => {
+        element.textContent = randomNumber(1,9999);
+    })
+    answersButtons[randomNumber(0,4)].textContent = questions.question[currentQuestion].correctAnswer;
+}
+
+const displayQuestion = () => {
+    questionNumber.innerText = questionNumberMessage();
+    questionMessage.innerText = questionMessagePrint();
+    generateRandomAnswers();
+}
+
+const disableAnswers = () => {
+    answersButtons.forEach((element) => {
+        element.disabled;
+    })
+}
+
+const checkAnswerAndCorrect = (event) => {
+    if (event.textContent === Number(questions.question[currentQuestion].correctAnswer)){
+        questionMessage.innerText = `${questionMessage.innerText} and the answer is correct `;
+        disableAnswers();
+        ScoreMenu.correctAnswers += 1;
+    } else {
+        questionMessage.innerText = `${questionMessage.innerText} and the answer is wrong the correct one is ${questions.question[currentQuestion].correctAnswer}`;
+        disableAnswers();
+        ScoreMenu.wrongAnswers += 1;
+    }
+}
+
+const checkWinner = () => {
+    if (ScoreMenu.correctAnswers >= ScoreMenu.wrongAnswers) {
+
+    }
+}
+
 /*----------- Event Listeners ----------*/
+
+startMenu.addEventListener('click', (event) => {
+    
+    if (event.target.classList.contains('operator')) {
+        ScoreMenu.operator = event.target.textContent;
+    } 
+
+    if (event.target.classList.contains('difficulty')) {
+        ScoreMenu.difficulty = event.target.textContent;
+    } 
+
+    if (event.target.classList.contains('start')) {
+
+        ScoreMenu.rounds = Number(rounds.value);
+
+        if (ScoreMenu.difficulty === undefined || ScoreMenu.operator === undefined || ScoreMenu.rounds === undefined) {
+            
+            message.innerText = 'please choose settings';
+        } 
+        
+        else {
+            generateQuestions();
+            startMenu.style.display = 'none';
+            displayQuestion();
+        }
+    }
+})
+
+questionSection.addEventListener('click', (event) => {
+
+    if (event.target.classList.contains('answer')) {
+        checkAnswerAndCorrect(event.target);
+    } 
+    if (event.target.classList.contains('next')) {
+        currentQuestion++;
+        if (currentQuestion>ScoreMenu.rounds){
+
+        }
+        displayQuestion();
+    }
+
+})
